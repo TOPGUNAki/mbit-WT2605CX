e9d1d56145696dae42bc7c0db6b2def274c49597
TOPGUN_Aki <aki.primary@gmail.com>
2025-11-16 14:37:53 +0900

    minimum structure of extension

---
+++ main.ts
@@ -0,0 +1,12 @@
+//% color=#00AACC icon=""
+namespace myext {
+
+    /**
+     * Hello を返す最小ブロック
+     */
+    //% block="hello"
+    export function hello(): string {
+        return "Hello";
+    }
+}
+
