82c81e71ddd6c360aa8e3645bd04ad21587b9d8d
TOPGUN_Aki <aki.primary@gmail.com>
2025-11-16 15:27:56 +0900

    minimum structure of extension

--- main.ts
+++ main.ts
@@ -1,23 +1,6 @@
-//% color=#ff8000 icon="\uf001"
-//% block="WT2605CX"
-namespace WT2605CX {
-
-    /**
-     * トラック番号を指定して再生
-     */
-    //% block="WT2605CX トラック %track を再生"
-    //% track.min=1 track.max=255
-    export function play(track: number): void {
-        // ここに実際の送信処理を書く
-    }
-}
-
 //% color=#00AACC icon=""
-namespace myext {
+namespace myextension {
 
-    /* *
-     * Hello を返す最小ブロック
-     */
     //% block="hello"
     export function hello(): string {
         return "Hello";
