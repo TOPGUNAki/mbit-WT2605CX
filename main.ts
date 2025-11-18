c3a7957d549f25aa118cb7c470625d35ffb69a92
TOPGUN_Aki <aki.primary@gmail.com>
2025-11-18 09:38:37 +0900

    set baud 115200

--- main.ts
+++ main.ts
@@ -6,20 +6,18 @@ let _initialized = false
      * WT2605CX モジュールを UART で初期化する
      * TX/RX はモジュールに接続したピンを選択してください
      */
-    //% block="WT2605CX を初期化 TX %tx RX %rx ボーレート %baud"
+    //% block="WT2605CX を初期化 TX %tx RX %rx"
     //% tx.defl=SerialPin.P1
     //% rx.defl=SerialPin.P2
-    //% baud.defl=BaudRate.BaudRate9600
     export function init(
         tx: SerialPin,
         rx: SerialPin,
-        baud: BaudRate
     ): void {
         // micro:bit のシリアル出力を指定ピンにリダイレクト
         serial.redirect(
             tx,
             rx,
-            baud
+            BaudRate.BaudRate115200
         )
 
         // 余裕を見て少し待つ（モジュール起動待ちなど）
